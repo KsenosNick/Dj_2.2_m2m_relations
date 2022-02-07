88650f0e960c9dceb96141269988993b3868c29f
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-07 18:45:42 +0300

    Созданы модели Article, Scope, ArticleScope

--- articles/models.py
+++ articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +14,22 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, through='ArticleScope', related_name='scopes')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='articles', verbose_name='Статья')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
