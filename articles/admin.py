bb710dd1a2e375b6e4d8c4f30dffb9b1b8a26577
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-07 18:51:33 +0300

    Настроена админка для моделей Article и Scope

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Scope, ArticleScope
+
+
+@admin.register(Scope)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class ScopeInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 0
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_at']
+    inlines = [ScopeInline]
+
