e9b36c5653b0abdaaa6bef59e07e06c1dce040f9
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-07 20:22:13 +0300

    Предварительно переопределен атрибут formset в объекте Inline

--- articles/admin.py
+++ articles/admin.py
@@ -10,8 +10,19 @@ class ScopeAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if len(self.forms) == 0:
+            raise ValidationError('Не указан основной раздел!')
+
+        if len(self.forms) != len({form.cleaned_data['scope'].id for form in self.forms}):
+            raise ValidationError('Основной раздел может быть только один!')
+        return super().clean()
+
+
 class ScopeInline(admin.TabularInline):
     model = ArticleScope
+    formset = ScopeInlineFormset
     extra = 0
 
 
